0a10b11a124a7a4cc4d0d125de75d303f271981a
Luciano <luciano.silva380@gmail.com>
2020-08-02 16:56:00 -0300

    first changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import Carrousel from './components/Carousel'
+import dadosIniciais from './data/dados_iniciais.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Menu />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front? Trabalhando na áreaO que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"} />
+
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
     </div>
   );
 }
