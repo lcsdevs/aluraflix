10ef1b16683688a35b578772317fd0ef4d3946c5
Luciano <luciano.silva380@gmail.com>
2020-08-06 00:38:34 -0300

    add category form page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Home from './pages/home/Home';
 import CadastroVideo from './pages/cadastro/video';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroCategoria from './pages/cadastro/categoria';
 
 
 const page404 = () => (<div>Not Found</div>)
@@ -13,7 +14,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={page404} />
     </Switch>
   </BrowserRouter>,
